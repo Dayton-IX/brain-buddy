20372c55bd37244d8af5e0ced8d70bced5b88b7a
Max Dayton <daytonmaximus@gmail.com>
2023-05-06 16:31:04 +0200

    filename formatting

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,7 @@ use chrono::{Datelike, Utc};
 
 fn main() -> std::io::Result<()> {
     let now = Utc::now();
+    let date_str = now.format("%a, %d %b %Y").to_string();
     let dir_path = Path::new("journal-entries");
     if !dir_path.exists() {
         std::fs::create_dir(dir_path)
@@ -15,13 +16,15 @@ fn main() -> std::io::Result<()> {
     let mut entry = String::new();
     println!(
         "Journal Entry for {}",
-        now.to_rfc2822()
+        date_str
     );
     io::stdin()
         .read_line(&mut entry)
         .expect("Failed to read entry");
 
-    let file_path = dir_path.join("test-entry.txt");
+    let mut file_name = date_str.to_lowercase().replace(" ", "-").replace(",", "");
+    file_name.push_str(".txt");
+    let file_path = dir_path.join(file_name);
     File::create(file_path)
         .expect("Failed to create file")
         .write_all(entry.as_bytes())
