02f2172d3c0550d5809e9a2e869eed80b69d2d09
Max Dayton <daytonmaximus@gmail.com>
2023-05-06 16:21:56 +0200

    show datetime and create entry in journal-entries directory

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,31 @@
 use std::fs::File;
 use std::io;
 use std::io::Write;
+use std::path::Path;
+use chrono::{Datelike, Utc};
 
-fn main() {
-    println!("Journal Entry for <today's date>:");
+fn main() -> std::io::Result<()> {
+    let now = Utc::now();
+    let dir_path = Path::new("journal-entries");
+    if !dir_path.exists() {
+        std::fs::create_dir(dir_path)
+            .expect("Failed to create directory");
+    }
 
     let mut entry = String::new();
-    
+    println!(
+        "Journal Entry for {}",
+        now.to_rfc2822()
+    );
     io::stdin()
         .read_line(&mut entry)
         .expect("Failed to read entry");
 
-    File::create("entry.txt")
+    let file_path = dir_path.join("test-entry.txt");
+    File::create(file_path)
         .expect("Failed to create file")
         .write_all(entry.as_bytes())
         .expect("Failed to write file");
+
+    Ok(())
 }
