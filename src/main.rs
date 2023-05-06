c4fd7d02d9109c325cf31db56daced6433dac9b9
Max Dayton <daytonmaximus@gmail.com>
2023-05-06 15:00:51 +0200

    write to txt file

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
+use std::fs::File;
 use std::io;
+use std::io::Write;
 
 fn main() {
     println!("Journal Entry for <today's date>:");
@@ -10,4 +12,7 @@ fn main() {
         .expect("Failed to read entry");
 
     println!("Journal Entry: {}", entry);
+
+    let mut file = File::create("entry.txt");
+    file.expect("Failed to save entry").write_all(entry.as_bytes());
 }
